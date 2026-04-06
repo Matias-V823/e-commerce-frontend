34502a5f737b66a87f0dba58e73378ce12a7c58b
Matias-V823 <matias.varas62@gmail.com>
2026-04-06 06:59:04 -0400

    feat: Replace category names with links in NavSection and improve category fetching error handling

--- app/ui/NavSection.tsx
+++ app/ui/NavSection.tsx
@@ -1,15 +1,21 @@
 import { getCategories } from "@/src/services/categories"
+import Link from "next/link"
+import { redirect } from "next/navigation"
 
 const NavSection = async () => {
 
     const categories = await getCategories()
 
-    console.log(categories)
-
     return (
         <div className="flex justify-center mb-3 gap-10">
             {categories.map((category) => (
-                <span key={category.id} className="cursor-pointer uppercase font-light">{category.name}</span>
+                <Link
+                    key={category.id}
+                    href={`/${category.id}`}
+                    className="cursor-pointer uppercase font-light"
+                >
+                    {category.name}
+                </Link>
             ))}
         </div>
     )
--- src/services/categories.ts
+++ src/services/categories.ts
@@ -1,15 +1,20 @@
 import { CategoriesArraySchema, CategoryWithProductsResponseSchema } from "@/app/schemas"
+import { redirect } from "next/navigation"
 
 const url = `${process.env.API_URL}/categories`
 
-export async function getProducts(categoryId: string){
+export async function getProducts(categoryId: string) {
   const req = await fetch(`${url}/${categoryId}?products=true`)
+  if (!req.ok) {
+    redirect('/1')
+  }
   const json = await req.json()
   const products = CategoryWithProductsResponseSchema.parse(json)
+
   return products
 }
 
-export async function getCategories(){
+export async function getCategories() {
   const req = await fetch(url)
   const json = await req.json()
   const categories = CategoriesArraySchema.parse(json)
